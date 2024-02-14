bcdecbe86383619245ce354fb8e1e75e72d95c7d
Abdul Majid <abdulmajidcse@gmail.com>
2024-02-14 18:49:56 +0600

    Added short url migration and added a new column in users table for admin.

---
+++ app/Models/ShortUrl.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ShortUrl extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'main_url',
+        'short_url_path',
+        'click_count',
+    ];
+}
